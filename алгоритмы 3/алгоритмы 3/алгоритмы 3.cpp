2c3508313a11b8609ec37c53c6140ddd172d495e
Станислав Шушков <80702083+Mazan-ka@users.noreply.github.com>
2021-11-07 16:05:43 +0300

    Practica №3

---
+++ алгоритмы 3/алгоритмы 3/алгоритмы 3.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+using namespace std;    
+
+int main()
+{
+	string email_address;
+	bool flag = false;
+	setlocale(LC_ALL, "Russian");
+	cout << "Введите адрес почты: ";
+	cin >> email_address;
+	int size = email_address.size();
+	for (int i=0; i<size; i++)
+	{
+		if (email_address[i] == '@') flag = true;
+		if (email_address[i+1] == '.' && flag)
+		{
+			if (email_address[i] == '-') {
+				cout << "Некоректный ввод";
+				return 0;
+			}
+		}
+	}
+	cout << "Корректный ввод";
+	return 0;
+}
